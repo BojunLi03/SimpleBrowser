ec9999bf05c9f702db5ad01f87b1d73e5e3949fd
BojunLi03 <bojun.li.03@gmail.com>
2024-11-11 21:33:08 -0500

    Initial Commit

---
+++ SimpleBrowser/SimpleBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBrowserApp.swift
+//  SimpleBrowser
+//
+//  Created by Bojun Li on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
